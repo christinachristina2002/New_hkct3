109db5a1d84b9c835689071f7201ac39b6567c24
christinachoi <christinachristina2002@gmail.com>
2024-08-31 12:23:07 +0800

    Step 2: create page app - Hello World

--- bcre/settings.py
+++ bcre/settings.py
@@ -36,10 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', #css file
+    'pages.apps.PagesConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [ #check login auths
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,9 +50,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bcre.urls'
+ROOT_URLCONF = 'bcre.urls' #Root URL
 
-TEMPLATES = [
+TEMPLATES = [ #Route URL
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
@@ -103,19 +104,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us' #website language
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC' #UK
 
-USE_I18N = True
+USE_I18N = True #translate
 
-USE_TZ = True
+USE_TZ = True #time zone
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' #local website - logo / background Image
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
