c82dd92728fdd9ffe40cebb5c8a6b78bb53bc11f
christinachoi <christinachristina2002@gmail.com>
2024-09-09 17:01:46 +0800

    Step 11: realtor related code

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from listings.models import Listing #<-- load listings\models.py
+from realtors.models import Realtor #<-- load realtors\models.py
+#from django.http import HttpResponse <-- load HTML
 
 # Create your views here.
 
 #Page: Pages/Index.html
 def index(request):
     #return HttpResponse("<h1>Hello World<h1>")
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
+    context = {'listings':listings}
+    return render(request, 'pages/index.html', context)
 
 #Page: Pages/About.html
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by("-hire_date")
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
+
